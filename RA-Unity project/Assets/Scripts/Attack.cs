3a10e3f2e56ff2915068f7270f8b93237c536d20
manelmourelo <manelmourelo.1@gmail.com>
2019-05-18 19:36:45 +0200

    Attack with raycast and go back to idle

--- RA-Unity project/Assets/Scripts/Attack.cs
+++ RA-Unity project/Assets/Scripts/Attack.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+
+    public LayerMask enemies;
+    private float timer = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, enemies) || Input.GetKeyDown(KeyCode.Return))
         {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
             gameObject.GetComponent<Animator>().SetBool("Attack", true);
+            timer = 0.0f;
         }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+
+        timer += Time.deltaTime;
+
+        if (timer >= 1.0f)
+        {
+            gameObject.GetComponent<Animator>().SetBool("Attack", false);
+        }
+
     }
 }
